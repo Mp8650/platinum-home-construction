06a2c322c2bfe73ef8a6312d502ebd8d05e355a1
Mahesh Pal <112304732+Mp8650@users.noreply.github.com>
2026-03-26 21:45:16 +0530

    video section hidden

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Services from "@/src/components/sections/Services";
 import Sustainability from "@/src/components/sections/Sustainability";
 import CTA from "@/src/components/sections/CTA";
 import ServicesSlider from "@/src/components/sections/ServicesSlider";
-import VideoShowcase from "@/src/components/sections/Videos";
+// import VideoShowcase from "@/src/components/sections/Videos";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
       <Stats />
       <ServicesSlider/>
       <Services />
-      <VideoShowcase/>
+      {/* <VideoShowcase/> */}
       <Sustainability />
       <CTA />
     
